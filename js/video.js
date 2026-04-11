34cbcedc29f4903885862c8da941a6aff25d6fda
MalYme <49.132134a@gmail.com>
2026-04-11 13:26:34 +0200

    Refactor quiz functionality: streamline button response handling and update scoring logic

--- js/video.js
+++ js/video.js
@@ -1,5 +1,5 @@
 var score = 0
-
+var score_total = 0
 function buttonStart(){
 
     window.open("video.html",'_self');
@@ -8,81 +8,12 @@ function buttonStart(){
 
 // 1ère Q° du quiz 
 
-function buttonreponse1A() {
-    score = score + 1
-    alert("réponse correct, score = "+score)
+function buttonreponseB() {
+    score ++
+    score_total ++
+    alert("réponse correct, score = "+score+"/"+score_total)
 }
-function buttonreponse1B() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse1C() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse1D() {
-    alert("réponse fausse, score = "+score)
-}
-
-
-// 2eme Q° du quiz 
-
-function buttonreponse2A() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse2B() {
-    score = score + 1
-    alert("réponse correct, score = "+score)
-}
-function buttonreponse2C() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse2D() {
-    alert("réponse fausse, score = "+score)
-}
-
-// 3ème Q° du quiz
-
-function buttonreponse3A() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse3B() {
-    score = score + 1
-    alert("réponse correct, score = "+score)
-}
-function buttonreponse3C() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse3D() {
-    alert("réponse fausse, score = "+score)
-}
-
-// 4eme Q° du quiz
-
-function buttonreponse4A() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse4B() {
-    score = score + 1
-    alert("réponse correct, score = "+score)
-}
-function buttonreponse4C() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse4D() {
-    alert("réponse fausse, score = "+score)
-}
-
-// 5eme Q° du quiz
-
-function buttonreponse5A() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse5B() {
-    score = score + 1
-    alert("réponse correct, score = "+score)
-}
-function buttonreponse5C() {
-    alert("réponse fausse, score = "+score)
-}
-function buttonreponse5D() {
-    alert("réponse fausse, SCORE TOTAL = "+score)
+function buttonreponseF() {
+    score_total ++
+    alert("réponse fausse, score = "+score+"/"+score_total)
 }
